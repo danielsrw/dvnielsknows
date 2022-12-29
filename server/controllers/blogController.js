44b125fd9cbbf65b46535fdf0bac67f763de0698
Andela Rwanda <danielsrwanda@gmail.com>
2022-12-30 01:13:29 +0200

    update

--- server/controllers/blogController.js
+++ server/controllers/blogController.js
@@ -82,7 +82,9 @@ exports.viewBlog = async(req, res) => {
     try {
         let blogId = req.params.id;
         const blog = await Blog.findById(blogId);
-        res.render('blog', { title: 'DvnielKnows - View Blog', blog } );
+        const categories = await Category.find({}).limit(5);
+
+        res.render('blog', { title: 'DvnielKnows - View Blog', blog, categories } );
     } catch (error) {
         res.satus(500).send({message: error.message || "Error Occured" });
     }
@@ -99,3 +101,75 @@ exports.searchBlog = async(req, res) => {
         res.satus(500).send({message: error.message || "Error Occured" });
     }
 }
+
+exports.exploreLatest = async(req, res) => {
+    try {
+        const limitNumber = 20;
+        const blog = await Blog.find({}).sort({ _id: -1 }).limit(limitNumber);
+        let categories = await Category.find({}).limit(5);
+
+        res.render('explore-latest', { title: 'DvnielKnows - Explore Latest', blog, categories } );
+    } catch (error) {
+        res.satus(500).send({message: error.message || "Error Occured" });
+    }
+}
+
+exports.exploreRandom = async(req, res) => {
+    try {
+        let count = await Blog.find().countDocuments();
+        let random = Math.floor(Math.random() * count);
+        let blog = await Blog.findOne().skip(random).exec();
+        let categories = await Category.find({}).limit(5);
+
+        res.render('explore-random', { title: 'DvnielKnows - Explore Latest', blog, categories } );
+    } catch (error) {
+        res.satus(500).send({message: error.message || "Error Occured" });
+    }
+}
+
+exports.submitBlog = async(req, res) => {
+    const infoErrorsObj = req.flash('infoErrors');
+    const infoSubmitObj = req.flash('infoSubmit');
+
+    res.render('submit-blog', { title: 'DvnielKnows - Submit Blog', infoErrorsObj, infoSubmitObj  } );
+}
+
+exports.submitBlogOnPost = async(req, res) => {
+    try {
+  
+        let imageUploadFile;
+        let uploadPath;
+        let newImageName;
+  
+      if(!req.files || Object.keys(req.files).length === 0){
+            console.log('No Files where uploaded.');
+      } else {
+        imageUploadFile = req.files.image;
+        newImageName = Date.now() + imageUploadFile.name;
+
+        uploadPath = require('path').resolve('./') + '/public/uploads/' + newImageName;
+
+        imageUploadFile.mv(uploadPath, function(err){
+            if(err) return res.satus(500).send(err);
+        })
+      }
+  
+      const newBlog = new Blog({
+            name: req.body.name,
+            description: req.body.description,
+            username: req.body.username,
+            tags: req.body.tags,
+            category: req.body.category,
+            image: newImageName
+      });
+      
+        await newBlog.save();
+    
+        req.flash('infoSubmit', 'Blog has been added.')
+        res.redirect('/submit-blog');
+    } catch (error) {
+        // res.json(error);
+        req.flash('infoErrors', error);
+        res.redirect('/submit-blog');
+    }
+  }
--- server/routes/blogRoutes.js
+++ server/routes/blogRoutes.js
@@ -13,7 +13,12 @@ router.get('/contact', blogController.contact);
 router.get('/register', blogController.register);
 router.get('/blog/:id', blogController.viewBlog);
 router.post('/search', blogController.searchBlog);
+router.get('/submit-blog', blogController.submitBlog);
 router.get('/categories', blogController.exploreCategories);
+router.get('/explore-latest', blogController.exploreLatest);
+router.get('/explore-random', blogController.exploreRandom);
+router.post('/submit-blog', blogController.submitBlogOnPost);
 router.get('/categories/:id', blogController.exploreCategoriesById);
+
  
 module.exports = router;
